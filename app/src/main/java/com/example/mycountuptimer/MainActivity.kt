45181bb6160a340e329400fa0ce5cdb5614776fc
c18003 <ohs3126@gmail.com>
2020-01-26 23:16:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mycountuptimer/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.mycountuptimer
+
+//import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val timeText = findViewById(R.id.timeText) as TextView
+        val startButton = findViewById(R.id.start) as Button
+        val stopButton = findViewById(R.id.stop) as Button
+        val resetButton = findViewById(R.id.reset) as Button
+
+        val handler = Handler()
+        var timeValue = 0
+
+        val runnable = object : Runnable {
+
+            override fun run() {
+                timeValue++
+                timeToText(timeValue)?.let {
+                    timeText.text = it
+                }
+                handler.postDelayed(this, 1000)
+            }
+        }
+
+
+        start.setOnClickListener {
+            handler.post(runnable)
+        }
+
+        stop.setOnClickListener {
+            handler.removeCallbacks(runnable)
+        }
+
+        reset.setOnClickListener {
+            handler.removeCallbacks(runnable)
+            timeValue = 0
+            timeToText()?.let {
+                timeText.text = it
+            }
+        }
+    }
+
+    private fun timeToText(time: Int = 0): String? {
+        return if (time < 0) {
+            null
+        } else if (time == 0) {
+            "00:00:00"
+        } else {
+            val h = time / 3600
+            val m = time % 3600 / 60
+            val s = time % 60
+            "%1$02d:%2$02d:%3$02d".format(h, m, s)
+        }
+    }
+
+}
+
